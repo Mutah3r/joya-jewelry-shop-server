1ced2c483115856015c44749b0745288289732fa
Mutah3r <evil.programmer.stephen@gmail.com>
2023-10-09 02:00:02 +0600

    Add user register routes

--- index.js
+++ index.js
@@ -26,10 +26,61 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
+        const database = client.db('JoyaJewelry');
+        const usersCollection = database.collection('users');
+
+
+        // save user info when user registers using email and password
+        app.put('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const name = req.body.name;
+            const photoURL = req.body.photoURL;
+            const gender = req.body.gender;
+            const phone = req.body.phone;
+            const address = req.body.address;
+
+            const query = { email: email }
+            const options = { upsert: true }
+            
+            const updateDoc = {
+                $set: {
+                    name: name,
+                    email: email,
+                    photoURL: photoURL,
+                    gender: gender,
+                    phone: phone,
+                    address: address,
+                }
+            }
+            
+            const result = await usersCollection.updateOne(query, updateDoc, options);
+
+            res.send(result);
+        });
+
+        // save user info when user logs in using google
+        app.put('/users/google/:email', async (req, res) => {
+            const email = req.params.email;
+            
+            const query = { email: email }
+            const options = { upsert: true }
+            
+            const updateDoc = {
+                $set: {
+                    email: email
+                }
+            }
+            
+            const result = await usersCollection.updateOne(query, updateDoc, options);
+            console.log(result);
+            res.send(result);
+        });
 
         app.get('/', (req, res) => {
             res.send('Hello World!')
-        })
+        });
+
+
 
 
         // Send a ping to confirm a successful connection
